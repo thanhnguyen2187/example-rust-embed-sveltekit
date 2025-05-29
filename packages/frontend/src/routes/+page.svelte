e79274a1e409812d2ba7f2b02c1c61e229f42419
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-05-29 16:19:23 +0700

    frontend/feat: make SPA building works

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -1,4 +1,16 @@
+<script lang="ts">
+async function fetchData() {
+  const resp = await fetch("/api/data");
+  return await resp.text();
+}
+</script>
+
 <div class="prose max-w-none">
     <h1>Example Rust Embed SvelteKit Project</h1>
-    <p>Hello world!</p>
+
+    {#await fetchData()}
+        Loading... (should take precisely 3 seconds)
+    {:then data}
+        {data}
+    {/await}
 </div>
--- packages/frontend/svelte.config.js
+++ packages/frontend/svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: "index.html",
+    }),
+  },
 };
 
 export default config;
