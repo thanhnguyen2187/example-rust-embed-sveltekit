2c40cc06c1197003eb8baf231ba7b360e75d4a46
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-05-29 16:19:46 +0700

    backend/feat: make embedding frontend work

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod frontend;
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route(
+            "/api/data",
+            get(async || {
+                tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+                "some arbitrary data"
+            }),
+        )
+        .fallback(frontend::static_handler);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
